2b2ee57e5a6e9e1e169c85823d17977fb60fd0de
XIONG <1351655382@qq.com>
2022-09-08 23:57:59 +0800

    product商品管理后台

---
+++ gulimall-common/src/main/java/com/atguigu/common/MyCommonApplication.java
@@ -0,0 +1,11 @@
+package com.atguigu.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyCommonApplication.class,args);
+    }
+}
--- gulimall-common/src/main/java/com/atguigu/common/xss/SQLFilter.java
+++ gulimall-common/src/main/java/com/atguigu/common/xss/SQLFilter.java
@@ -8,7 +8,6 @@
 
 package com.atguigu.common.xss;
 
-import com.xunqi.common.utils.RRException;
 import org.apache.commons.lang.StringUtils;
 
 /**
@@ -41,7 +40,7 @@ public class SQLFilter {
         //判断是否包含非法字符
         for(String keyword : keywords){
             if(str.indexOf(keyword) != -1){
-                throw new RRException("包含非法字符");
+                System.out.println("包含非法字符");
             }
         }
 
